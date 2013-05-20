4ef9401c76d7544e2156aec130a614f540c83c70
Taylor Boyko <taylorboyko@gmail.com>
2013-05-20 11:37:31 -0700

    Revert "enhanced APNS format support"
    This reverts commit b9d8042be57a321d76e26a67161b9db74272978e.

--- lib/apple_shove/notification.rb
+++ lib/apple_shove/notification.rb
@@ -18,25 +18,10 @@ module AppleShove
     end
     
     # Apple APNS format
-    def binary_message(msgid = nil, expiration = nil)
+    def binary_message
       payload_json  = @payload.to_json
-
-	if !msgid
-		message = [0, 32, @device_token, payload_json.length, payload_json]
-		packed = message.pack('CnH*na*')
-	else
-		msgid = 256 + Random.rand(100)	# Maybe this should be an attr_accessor
-		expiration ||= 1400595627	# Maybe this should be an attr_accessor
-		message       = [1, msgid.to_i, expiration.to_i, 32, @device_token, payload_json.length, payload_json]
-		packed = message.pack("CNNnH*na*")
-	end
-
-	#print "\nMSG BINARY: "
-	#print packed
-	#print "\nMSG CHR: "
-	#print packed.unpack("C*") 
-	#print "\n"
-	packed
+      message       = [0, 32, @device_token, payload_json.length, payload_json]
+      message.pack('CnH*na*')
     end
     
     private
