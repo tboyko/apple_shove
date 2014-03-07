3ae93fe5d42bd270804bdc5f47eec97da8df1fe4
Taylor Boyko <taylorboyko@gmail.com>
2014-03-07 15:28:44 -0800

    fixed tests

--- spec/notification_queue_spec.rb
+++ spec/notification_queue_spec.rb
@@ -13,21 +13,30 @@ describe AppleShove::NotificationQueue do
   end
 
   it 'should add notifications to the queue' do
+    expect_any_instance_of(Redis).to receive(:rpush)
+
     n = generate_notification
     @q.add(n)
   end
 
   it 'should count notification on the queue when they are there' do
-    @q.size.should_not eql(0)
+    expect_any_instance_of(Redis).to receive(:llen).and_return(1)
+
+    expect(@q.size).to be_a_kind_of(Integer)
   end
 
   it 'should get notifications from the queue' do
-    while n = @q.get
-      n.should be_an_instance_of(AppleShove::Notification)
-    end
+    json = { p12: 'some p12', device_token: '123123123', payload: 'this is a test payload' }.to_json
+    expect_any_instance_of(Redis).to receive(:lpop).and_return(json)
+
+    notification = @q.get
+    
+    expect(notification).to be_an_instance_of(AppleShove::Notification)
   end
 
   it 'should count 0 notifications when the queue is empty' do
+    expect_any_instance_of(Redis).to receive(:llen).and_return(0)
+    
     @q.size.should eql(0)
   end
 
